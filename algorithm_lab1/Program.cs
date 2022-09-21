3614feaa153b42ba33aba71fd3108653db00d254
Amir <70907990+Amirka2@users.noreply.github.com>
2022-09-21 21:18:45 +0500

    Matrix multiplier added

--- algorithm_lab1/Program.cs
+++ algorithm_lab1/Program.cs
@@ -7,6 +7,14 @@ namespace algorithm_lab1
         private static void Main(string[] args)
         {
             int[] arr = Helper.getRandomArray(100);
+            int[,] arr1 = { { 1, 2, 2 }, { 3, 1, 1 } };
+            int[,] arr2 = { { 4, 2 }, { 3, 1 }, { 1, 5 } };
+            Matrix m1 = new Matrix(arr1);
+            Matrix m2 = new Matrix(arr2);
+            MatrixMultiplier matrixMultiplier = new MatrixMultiplier(m1, m2);
+            matrixMultiplier.Calculate();
+            matrixMultiplier.ResultMatrix.PrintMatrix();
+
             //Const con = new Const(arr);
             BubbleSort bS = new(arr);
             QuickSort qS = new(arr);
@@ -31,10 +39,10 @@ namespace algorithm_lab1
             //}
             //Console.WriteLine("quicksort");
 
-            foreach (int el in tS.Array)
-            {
-                Console.Write(el + ", ");
-            }
+            //foreach (int el in tS.Array)
+            //{
+            //    Console.Write(el + ", ");
+            //}
         }
     }
 }
