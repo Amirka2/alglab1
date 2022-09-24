020ce1654df8b1d2af0d98de3facb89ebbc9af88
Amir <70907990+Amirka2@users.noreply.github.com>
2022-09-24 12:27:59 +0500

    helper methods fixed

--- algorithm_lab1/Helper.cs
+++ algorithm_lab1/Helper.cs
@@ -7,7 +7,7 @@ namespace algorithm_lab1
 {
     public static class Helper
     {
-        static public void saveData(string path, string data)
+        static public void SaveData(string path, string data)
         {
             using (StreamWriter sw = new(path))
             {
@@ -15,7 +15,7 @@ namespace algorithm_lab1
             }
         }
 
-        static public string getData(string path)
+        static public string GetData(string path)
         {
             StringBuilder sb = new();
             using (StreamReader sr = new(path))
@@ -28,12 +28,12 @@ namespace algorithm_lab1
             return sb.ToString();
         }
 
-        static public void deleteData(string path)
+        static public void DeleteData(string path)
         {
             File.Delete(path);
         }
 
-        static public void deleteData(string[] paths)
+        static public void DeleteData(string[] paths)
         {
             for (int i = 0; i < paths.Length; i++)
             {
@@ -41,21 +41,40 @@ namespace algorithm_lab1
             }
         }
 
-        static public int[,] formatXandY(string data)
+        static public int[,] FormatXandY(string data) // time and n
         {
             string[] strArray = data.Split(" ");
             int rows = strArray.Length / 2;
-            int[,] coordinates = new int[2, rows];
+            int[,] coordinates = new int[rows, 2];
 
-            for(int i = 0; i < rows; i++) {
-                coordinates[0, i] = i;
-                coordinates[1, i] = i* 2;
+            for(int i = 0; i < rows; i++)
+            {
+                coordinates[i, 0] = Int32.Parse(strArray[i * 2]);
+                coordinates[i, 1] = Int32.Parse(strArray[i * 2 + 1]);
             }
 
             return coordinates;
         }
 
-        static public long makeMeasure(Algorithm algorithm)
+        static public void CreateCSVFile(string path, int[,] coordinates)
+        {
+            using (StreamWriter sw = new(path))
+            {
+                sw.WriteLine("n;time;");
+                int columns = coordinates.GetLength(0) + 1;
+                int rows = coordinates.Length / columns;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        sw.Write(coordinates[i,j] + ";");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static public int MakeMeasure(Algorithm algorithm, int n, int step)
         {
             int ticksPerMicrosecond = 10;
             Stopwatch stopWatch = new();
@@ -63,14 +82,14 @@ namespace algorithm_lab1
             stopWatch.Start();
             algorithm.Calculate();
             stopWatch.Stop();
-
+            
             long ticks = stopWatch.ElapsedTicks;
-            long microseconds = ticks / ticksPerMicrosecond;
+            int microseconds = (int)ticks / ticksPerMicrosecond;
 
             return microseconds;
         }
 
-        public static int[] getRandomArray(int length)
+        public static int[] GetRandomArray(int length)
         {
             int[] arr = new int[length];
             Random rnd = new();
@@ -85,7 +104,7 @@ namespace algorithm_lab1
             return arr;
         }
 
-        public static void printArray(int[] array)
+        public static void PrintArray(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
